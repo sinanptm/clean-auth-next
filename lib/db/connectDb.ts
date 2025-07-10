187d729bc12bfed3f9801de82924cdaf663401c9
muhammed sinan <muhammedsinan7749@gmail.com>
2025-07-10 20:18:34 +0530

    added schemas and deps

---
+++ lib/db/connectDb.ts
@@ -0,0 +1,16 @@
+import { MONGO_URI } from "@/config";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(MONGO_URI);
+        if (connection.readyState === 1) {
+            return Promise.resolve(true);
+        }
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+    }
+};
+
+export default connectDB;
---
+++ lib/db/models/User.ts
@@ -0,0 +1,18 @@
+import { IUser } from "@/types";
+import { Model, model, models, Schema } from "mongoose";
+
+const userSchema = new Schema<IUser>(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        isBlocked: { type: Boolean, default: false },
+        profile: { type: String },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+const UserModel: Model<IUser> = models?.User || model("User", userSchema);
+export default UserModel;
